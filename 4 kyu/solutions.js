0f9a80f05e2dd5b669112fe06abf6a76a130e6f1
ArtyomSamsonchik <colpachek@gmail.com>
2022-05-27 01:03:59 +0300

    Added 4 kyu solutions

--- 4 kyu/solutions.js
+++ 4 kyu/solutions.js
@@ -1,4 +1,4 @@
-//Smallest possible sum 
+//"Smallest possible sum "
 //"https://www.codewars.com/kata/52f677797c461daaf7000740/train/javascript"
 
 function solution(numbers) {
@@ -16,4 +16,39 @@ function solution(numbers) {
   });
   
   return gcd * numbers.length;
+}
+
+//"Codewars style ranking system"
+//""
+
+function User() {
+  this._range = [-8,-7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7,8];
+  this._index = 0;
+  this.rank = this._range[this._index];
+  this.progress = 0;
+  
+  this.incProgress = function (kataRank) {
+    if (kataRank < -8 || kataRank > 8 || kataRank === 0) throw new Error();
+    if (this.rank === 8) return;
+    
+    let diff = this._range.indexOf(kataRank) - this._index;
+    if (diff === 0) {
+      this.progress += 3;
+    } else if (diff === -1) {
+      this.progress += 1;
+    } else if (diff > 0) {
+      this.progress += 10 * diff * diff;
+    }
+    
+    if (this.progress >= 100) {
+      this._index += Math.trunc(this.progress / 100);
+      this.rank = this._range[this._index];
+      this.progress = this.progress % 100;
+      
+      if (this.rank >= 8) {
+        this.rank = 8;
+        this.progress = 0;
+      }
+    }
+  }
 }
